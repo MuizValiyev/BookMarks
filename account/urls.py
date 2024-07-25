baa60e375d714d2fe3dd2aff5043fe5d2b6bd59b
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 <valiyevmuiz0407@gmail.com>
2024-07-25 15:07:07 +0500

    fix

--- account/urls.py
+++ account/urls.py
@@ -1,10 +1,23 @@
-from django.urls import path
+from django.urls import path, include
 from .views import dashboard
-from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.views import (
+    LoginView, LogoutView, PasswordChangeDoneView, PasswordChangeView, PasswordResetView, PasswordResetView, PasswordResetDoneView, 
+    PasswordResetCompleteView, PasswordResetConfirmView)
 
 urlpatterns = [
-    # path('login/', user_login, name='login')
+    # path('', include('django.contrib.auth.urls')), 
+    path('', dashboard, name='dashboard'),
+    # path('login/', user_login, name='login'),
     path('login/', LoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
-    path('', dashboard, name='dashboard'),
+
+    
+    path('password-change/', PasswordChangeView.as_view(),name='password_change'),
+    path('password-change/done/', PasswordChangeDoneView.as_view(),name='password_change_done'),
+
+
+    path('password-reset/',PasswordResetView.as_view(),name='password_reset'),
+    path('password-reset/done/',PasswordResetDoneView.as_view(),name='password_reset_done'),
+    path('password-reset/<uidb64>/<token>/',PasswordResetConfirmView.as_view(),name='password_reset_confirm'),
+    path('password-reset/complete/',PasswordResetCompleteView.as_view(),name='password_reset_complete'),
 ]
--- bookmarks/settings.py
+++ bookmarks/settings.py
@@ -114,3 +114,10 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_REDIRECT_URL = 'dashboard'
 LOGIN_URL = 'login'
 LOGOUT_URL = 'logout'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'valiyevmuiz0407@gmail.com'
+EMAIL_HOST_PASSWORD = 'ystb uzjt xlbs jlfh'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
